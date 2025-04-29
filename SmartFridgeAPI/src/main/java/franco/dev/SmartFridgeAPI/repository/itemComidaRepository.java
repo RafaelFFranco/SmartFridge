de093918d6802d74c837951a61d38d5ca09eea3a
unknown <rafaelfariafranco30@gmail.com>
2025-04-29 20:28:24 -0300

    Feat: Add itemComida repository

---
+++ SmartFridgeAPI/src/main/java/franco/dev/SmartFridgeAPI/repository/itemComidaRepository.java
@@ -0,0 +1,7 @@
+package franco.dev.SmartFridgeAPI.repository;
+
+import franco.dev.SmartFridgeAPI.model.itemComida;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface itemComidaRepository extends JpaRepository<itemComida, Long> {
+}
