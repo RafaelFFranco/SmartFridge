39076e09e2c64401d79777b022232926a25f52a9
unknown <rafaelfariafranco30@gmail.com>
2025-04-29 19:58:25 -0300

    First commit

---
+++ SmartFridgeAPI/src/main/java/franco/dev/SmartFridgeAPI/SmartFridgeApiApplication.java
@@ -0,0 +1,13 @@
+package franco.dev.SmartFridgeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFridgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFridgeApiApplication.class, args);
+	}
+
+}
---
+++ SmartFridgeAPI/src/test/java/franco/dev/SmartFridgeAPI/SmartFridgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package franco.dev.SmartFridgeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFridgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
