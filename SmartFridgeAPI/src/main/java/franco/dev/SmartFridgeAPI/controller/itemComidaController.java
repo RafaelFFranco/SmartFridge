c29ea466325bf1867c31e66a7af15e3cf44fd08c
unknown <rafaelfariafranco30@gmail.com>
2025-04-29 20:27:02 -0300

    Feat: Add controller itemComida

---
+++ SmartFridgeAPI/src/main/java/franco/dev/SmartFridgeAPI/controller/itemComidaController.java
@@ -0,0 +1,10 @@
+package franco.dev.SmartFridgeAPI.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+public class itemComidaController {
+
+}
